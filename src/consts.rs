05385defa694d37e94f1b177c2bdee0b14accf34
r2 <ahmadolukotun@gmail.com>
2025-07-29 16:27:50 +0100

    added auth table names

---
+++ src/consts.rs
@@ -0,0 +1,11 @@
+pub mod auth_const {
+    pub const USER_TABLE: &str = "users";
+    pub const AUTH_PASSWORD_TABLE: &str = "auth_passwords";
+    pub const ORGANIZATION_TABLE: &str = "organizations";
+    pub const ORGANIZATION_MEMBERSHIP_TABLE: &str = "organization_memberships";
+    pub const TEAM_TABLE: &str = "teams";
+    pub const TEAM_MEMBERSHIP_TABLE: &str = "team_memberships";
+    pub const ROLE_TABLE: &str = "roles";
+    pub const INVITATION_TABLE: &str = "invitations";
+    pub const LOG_TABLE: &str = "logs";
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+pub mod consts;
 pub mod models;
 
 fn main() {
